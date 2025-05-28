6d263c0125b2a549bcc0f6f77d899ad5f277db46
Stephen Thompson <s.thompson@ucl.ac.uk>
2025-05-28 08:58:46 +0100

    First commit

---
+++ src/turtlebot4_first_cpp_node.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+
+int main(int argc, char ** argv)
+{
+  (void) argc;
+  (void) argv;
+
+  printf("hello world turtlebot4_cpp_tutorials package and a biscuit\n");
+  return 0;
+}

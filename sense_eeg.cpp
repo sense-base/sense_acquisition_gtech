c0ba0e9c6d541927ab05573305e32fa936f6e0e6
Stephen Thompson <s.thompson@ucl.ac.uk>
2025-06-26 10:10:26 +0100

    Implemented destructor and use class in main

--- sense_eeg.cpp
+++ sense_eeg.cpp
@@ -116,6 +116,16 @@ public:
         std::cout << "started" << std::endl;
 
     }
+    ~GtecEEGPublisher()
+    {
+        std::cout << "Stop DAQ ... ";
+        GT_StopAcquisition( master.c_str() );
+        std::cout << "stopped"  << std::endl;
+
+        GT_CloseDevice( master.c_str() );
+        fclose( data_file_master );
+    }
+
 
 private:
     void publish_data(void* dummy)
@@ -136,81 +146,9 @@ private:
     std::string serial_num_;
 };
 //------------------------------------------------------------------------------
-int main()
-{
-  GT_ShowDebugInformation( GT_TRUE );
-  master = MASTER;
-  data_file_master = fopen( "data_master.bin", "wb" );
-
-  const int sample_rate = SAMPLERATE;
-  gt_usbamp_analog_out_config ao_config_master;
-  ao_config_master.shape = GT_ANALOGOUT_SINE;
-  ao_config_master.frequency = 10;
-  ao_config_master.amplitude = 200;
-  ao_config_master.offset = 0;
-
-  gt_usbamp_config config_master;
-  config_master.ao_config = &ao_config_master;
-  config_master.sample_rate = sample_rate;
-  config_master.number_of_scans = GT_NOS_AUTOSET;
-  config_master.enable_trigger_line = GT_FALSE;
-  config_master.scan_dio = GT_FALSE;
-  config_master.slave_mode = GT_FALSE;
-  config_master.enable_sc = GT_FALSE;
-  config_master.mode = GT_MODE_NORMAL;
-  config_master.num_analog_in = 16;
-
-  gt_usbamp_asynchron_config asynchron_config_master;
-  for ( unsigned int i = 0; i < GT_USBAMP_NUM_GROUND; i++ )
-  {
-    asynchron_config_master.digital_out[i] = GT_FALSE;
-    config_master.common_ground[i] = GT_TRUE;
-    config_master.common_reference[i] = GT_TRUE;
-  }
-
-  for ( unsigned char i = 0; i < config_master.num_analog_in ; i++ )
-  {
-    config_master.analog_in_channel[i] = i + 1;
-    config_master.bandpass[ i ] = GT_FILTER_NONE;
-    config_master.notch[ i ] =  GT_FILTER_NONE;
-    config_master.bipolar[ i ] = GT_BIPOLAR_DERIVATION_NONE;
-  }
-
-  char** device_list = 0;
-  size_t list_size = 0;
-  GT_UpdateDevices();
-  list_size = GT_GetDeviceListSize();
-  device_list = GT_GetDeviceList();
-
-  GT_FreeDeviceList( device_list, list_size );
-
-  if ( GT_OpenDevice( master.c_str() ) )
-  {
-    std::cout << "Master : " << master << " open" << std::endl;
-  }
-  else
-  {
-    std::cout << "Could not open device " << master << std::endl;
-    fclose( data_file_master );
-    return -1;
-  }
-  if ( GT_SetConfiguration( master.c_str(), &config_master ) )
-  {
-    std::cout << "Master:  Applied config master." << std::endl;
-  }
-
-  int dummy_arg = 0;
-  GT_SetDataReadyCallBack( master.c_str(), &CallBackMaster, (void*)(&dummy_arg) );
-  std::cout << "Start DAQ ... ";
-  GT_StartAcquisition( master.c_str() );
-  std::cout << "started" << std::endl;
-
-
-  sleep( TIME );
-  std::cout << "Stop DAQ ... ";
-  GT_StopAcquisition( master.c_str() );
-  std::cout << "stopped"  << std::endl;
-
-  GT_CloseDevice( master.c_str() );
-  fclose( data_file_master );
+int main(int argc, char * argv[]) {
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<GtecEEGPublisher>());
+    rclcpp::shutdown();
+    return 0;
 }

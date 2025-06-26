b16da01e604fcb4f7112d50bec0e5aaea78d48a5
Stephen Thompson <s.thompson@ucl.ac.uk>
2025-06-26 09:41:28 +0100

    Fixed package names

--- launch/gtec_eeg_publisher_launch.py
+++ launch/gtec_eeg_publisher_launch.py
@@ -5,8 +5,8 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
-            package='sense_gtec_eeg',
-            executable='sense_eeg',
+            package='gtec_eeg_publisher',
+            executable='gtec_eeg_publisher',
             name='gtec_eeg_publisher',
             output='screen'
         ),

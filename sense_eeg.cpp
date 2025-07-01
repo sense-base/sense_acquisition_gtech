dc46b805ce886b79f65f6a6be706087a88a0cde9
Stephen Thompson <s.thompson@ucl.ac.uk>
2025-07-01 15:43:33 +0100

    moves publish data out of class and uses call back parameter to pass configuration data to the publish data function, which should make it possible to re-configure without re-compiling

--- sense_eeg.cpp
+++ sense_eeg.cpp
@@ -20,20 +20,58 @@ std::string master;
 unsigned char usr_buffer_master[ 32768 ];
 
 rclcpp::Publisher<eeg_msgs::msg::EEGBlock>::SharedPtr publisher_;
-int num_channels_ = 1;
-int num_samples_ = 2;
-float sampling_rate_ = 256;
-std::string serial_num_ = "UR-2017.06.12";
+
+class GtecEEGConfig {
+public:
+    int num_channels;
+    int num_samples;
+    float sampling_rate;
+    std::string serial_num;
+    rclcpp::Time now; // not in use
+};
+
+void publish_data(void* context)
+{
+    auto msg = eeg_msgs::msg::EEGBlock();
+    GtecEEGConfig* config = static_cast<GtecEEGConfig*>(context);
+    std::cout << "   " << config->num_channels << "     ";
+    //msg.header.stamp = this->now();
+    msg.header.stamp = rclcpp::Clock(RCL_ROS_TIME).now();  // calling this->now does not work (runtime error)
+    msg.num_channels = config->num_channels;
+    msg.num_samples = config->num_samples;
+    msg.sampling_rate = config->sampling_rate;
+
+    size_t cnt_master = GT_GetSamplesAvailable( master.c_str() );
+    msg.data.reserve(cnt_master);
+    //msg.data.reserve(cnt_master);
+
+    GT_GetData( master.c_str(), usr_buffer_master, cnt_master);
+    float* float_buffer = reinterpret_cast<float *>(usr_buffer_master);
+
+    for (size_t i = 0; i < cnt_master / 4; ++i) {
+        msg.data.push_back(float_buffer[i]);
+    }
+    //msg.data.data = usr_buffer_master
+    publisher_->publish(msg);
+    // RCLCPP_INFO(this->get_logger(), "Published EEGBlock with %ld samples", msg.data.size());
+}
 
 class GtecEEGPublisher : public rclcpp::Node {
 public:
     GtecEEGPublisher()
     : Node("gtec_eeg_publisher")
     {
-        GT_ShowDebugInformation( GT_TRUE );
-        master = serial_num_;
+    static GtecEEGConfig pub_config = GtecEEGConfig();
 
-        const int sample_rate = sampling_rate_;
+        pub_config.num_channels = 1;
+        pub_config.num_samples = 2;
+        pub_config.sampling_rate = 256;
+        pub_config.serial_num = "UR-2017.06.12";
+
+        GT_ShowDebugInformation( GT_TRUE );
+        master = pub_config.serial_num;
+
+        const int sample_rate = pub_config.sampling_rate;
         gt_usbamp_analog_out_config ao_config_master;
         ao_config_master.shape = GT_ANALOGOUT_SINE;
         ao_config_master.frequency = 10;
@@ -46,14 +84,14 @@ public:
         // number_of_scans sets how many scans are included in each message.
         // setting to GT_NOS_AUTOSET trys to automatically set a value that
         // balances packet size against CPU load. Lots of small messages creates
-       // high CPU load.
+        // high CPU load.
         config_master.number_of_scans = GT_NOS_AUTOSET;
         config_master.enable_trigger_line = GT_FALSE;
         config_master.scan_dio = GT_FALSE;
         config_master.slave_mode = GT_FALSE;
         config_master.enable_sc = GT_FALSE;
         config_master.mode = GT_MODE_NORMAL;
-        config_master.num_analog_in = num_channels_;
+        config_master.num_analog_in = pub_config.num_channels;
 
         for ( unsigned int i = 0; i < GT_USBAMP_NUM_GROUND; i++ )
         {
@@ -95,9 +133,10 @@ public:
         // qos_history_depth The depth of the publisher message queue
         publisher_ = this->create_publisher<eeg_msgs::msg::EEGBlock>("/eeg/raw", 10);
 
-        int dummy_arg = 0;
+        std::cout << "Config: " << pub_config.num_channels << " : " << pub_config.num_samples;
+        int dummy_arg = 7;
         // GT_SetDataReadyCallBack( master.c_str(), &GtecEEGPublisher::publish_data, (void*)(&dummy_arg)) ;
-        GT_SetDataReadyCallBack( master.c_str(), GtecEEGPublisher::publish_data_wrapper, (void*)(&dummy_arg)) ;
+        GT_SetDataReadyCallBack( master.c_str(), &publish_data, (void*)(&pub_config)) ;
         std::cout << "Start DAQ ... ";
         GT_StartAcquisition( master.c_str() );
         std::cout << "started" << std::endl;
@@ -110,34 +149,6 @@ public:
         std::cout << "stopped"  << std::endl;
 
         GT_CloseDevice( master.c_str() );
-    }
-
-    static void publish_data_wrapper(void* context) {
-        static_cast<GtecEEGPublisher*>(context)->publish_data();
-    }
-
-    void publish_data()
-    {
-        auto msg = eeg_msgs::msg::EEGBlock();
-        //msg.header.stamp = this->now();
-        msg.header.stamp = rclcpp::Clock(RCL_ROS_TIME).now();  // calling this->now does not work (runtime error)
-        msg.num_channels = num_channels_;
-        msg.num_samples = num_samples_;
-        msg.sampling_rate = sampling_rate_;
-
-        size_t cnt_master = GT_GetSamplesAvailable( master.c_str() );
-        msg.data.reserve(cnt_master);
-        //msg.data.reserve(cnt_master);
-
-        GT_GetData( master.c_str(), usr_buffer_master, cnt_master);
-        float* float_buffer = reinterpret_cast<float *>(usr_buffer_master);
-
-        for (size_t i = 0; i < cnt_master / 4; ++i) {
-            msg.data.push_back(float_buffer[i]);
-        }
-        //msg.data.data = usr_buffer_master
-        publisher_->publish(msg);
-        // RCLCPP_INFO(this->get_logger(), "Published EEGBlock with %ld samples", msg.data.size());
     }
 
    };

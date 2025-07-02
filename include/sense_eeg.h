e157e967e51adc9d2aa59fe0c010ff482f767079
Stephen Thompson <s.thompson@ucl.ac.uk>
2025-07-02 17:06:23 +0100

    More implementation ros node (#10)
    * moves publish data out of class and uses call back parameter to pass configuration data to the publish data function, which should make it possible to re-configure without re-compiling
    
    * Use publisher class for config to save on classes
    
    * Tidying up
    
    * Use logger more
    
    * Removed more cout and unused defines
    
    * Use a header file
    
    * Removed unnessary includes
    
    * Variable for queue depth
    
    * Clearer variable naming
    
    * Put the code in the src directory

---
+++ include/sense_eeg.h
@@ -0,0 +1,20 @@
+//------------------------------------------------------------------------------
+//Copyright 2026 University College London. Partly derived from main.cpp expample file
+//courtesy g.tec Copyright (C) 2014-2016 g.tec medical engineering GmbH.
+
+#include "rclcpp/rclcpp.hpp"
+#include "eeg_msgs/msg/eeg_block.hpp"
+
+unsigned char usr_buffer_master[ 32768 ];
+
+class GtecEEGPublisher : public rclcpp::Node {
+public:
+    GtecEEGPublisher();
+    ~GtecEEGPublisher();
+    int num_channels;
+    int num_samples;
+    float sampling_rate;
+    std::string serial_num;
+    rclcpp::Publisher<eeg_msgs::msg::EEGBlock>::SharedPtr publisher;
+};
+

396f71d7bdaaeec5b10575180899d06ff5e0f669
nikk-nikaznan <48319650+nikk-nikaznan@users.noreply.github.com>
2025-07-03 15:01:41 +0100

    Update src/sense_eeg.cpp
    Co-authored-by: Stephen Thompson <s.thompson@ucl.ac.uk>

--- src/sense_eeg.cpp
+++ src/sense_eeg.cpp
@@ -36,7 +36,6 @@ void publish_data(void* eeg_publisher)
 GtecEEGPublisher::GtecEEGPublisher() : Node("gtec_eeg_publisher"),
 
     num_channels(declare_parameter<int>("num_channels", 1)),
-    num_samples(declare_parameter<int>("num_samples", 1)),
     sampling_rate(declare_parameter<double>("sampling_rate", 256.0)),
     serial_num(declare_parameter<std::string>("serial_num", "UR-2017.06.12")),
     

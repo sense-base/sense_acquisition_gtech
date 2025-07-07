7dc47e8caa2852eeb914765912e00bca58dc6d8f
nikk-nikaznan <nik.k.nikaznan@outlook.com>
2025-07-07 16:01:09 +0100

    formatting

--- include/sense_eeg.h
+++ include/sense_eeg.h
@@ -11,23 +11,22 @@
 
 class GtecEEGPublisher: public rclcpp::Node {
 public:
-    GtecEEGPublisher();
-    ~GtecEEGPublisher();
-    int num_channels;
-    int sampling_rate;
-    std::string serial_num;
-    rclcpp::Publisher<eeg_msgs::msg::EEGBlock>::SharedPtr publisher;
+  GtecEEGPublisher();
+  ~GtecEEGPublisher();
+  int num_channels;
+  int sampling_rate;
+  std::string serial_num;
+  rclcpp::Publisher < eeg_msgs::msg::EEGBlock > ::SharedPtr publisher;
 
 private:
-    int number_of_scans;
-    int ao_frequency;
-    int ao_amplitude;
-    int ao_offset;
-    bool enable_trigger_line;
-    bool scan_dio;
-    bool slave_mode;
-    bool enable_sc;
-    
+  int number_of_scans;
+  int ao_frequency;
+  int ao_amplitude;
+  int ao_offset;
+  bool enable_trigger_line;
+  bool scan_dio;
+  bool slave_mode;
+  bool enable_sc;
 
 };
 
